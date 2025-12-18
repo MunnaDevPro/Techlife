c82b9bf33a5ba7d9a9155f27713fd387201d2856
Md Munna <munna.codeexpert@gmail.com>
2025-12-18 19:05:55 +0600

    complete all forum section

--- forum/urls.py
+++ forum/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from forum.views import questions_list, questions, post_answer
+from forum.views import questions_list, questions, post_answer, create_question
 
 
 urlpatterns = [
     path("all_question/",questions_list, name="questions_list"),
     path("questions/<slug:slug>/",questions, name="questions"),
-    path("question/<slug:slug>/answer/",post_answer,name="post_answer")
+    path("question/<slug:slug>/answer/",post_answer,name="post_answer"),
+
+    path("ask-question/",create_question, name="create_question")
 
 ]
--- forum/views.py
+++ forum/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from blog_post.models import BlogPost
 from forum.models import Question, Answer
 from django.shortcuts import redirect, get_object_or_404
-from django.db.models import Q
+from django.db.models import Q, Count
 from django.core.paginator import Paginator
-
+from django.contrib.auth.decorators import login_required
 
 def questions(request, slug):
     blogs = BlogPost.objects.all().order_by('-created_at')
@@ -53,6 +53,28 @@ def questions(request, slug):
 def questions_list(request):
     blogs = BlogPost.objects.all().order_by("-created_at")
     questions = Question.objects.select_related('author').prefetch_related('answers').all()    
+
+
+    query = request.GET.get('q')
+    if query:
+        questions = questions.filter(
+            Q(title__icontains=query) | Q(content__icontains=query)
+        )
+
+
+    sort = request.GET.get('sort', 'latest')
+    if sort == 'top':
+        questions = questions.annotate(ans_count=Count('answers')).order_by('-ans_count')
+    elif sort == 'best':
+        questions = questions.annotate(ans_count=Count('answers')).order_by('-ans_count', '-created_at')
+    elif sort == 'new' or sort == 'latest':
+        questions = questions.order_by("-created_at")
+  
+
+    paginator = Paginator(questions, 2) 
+    page_number = request.GET.get('page')
+    questions = paginator.get_page(page_number)
+
     context = {
         "blogs":blogs,
         "questions" :questions,
@@ -76,3 +98,23 @@ def post_answer(request, slug):
         
     return redirect('questions', slug=slug)
 
+def create_question(request):
+
+    if not request.user.is_authenticated:
+        return redirect("questions_list")
+    
+    if request.method == "POST":
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        image = request.FILES.get('image')
+
+    
+        if title and content:
+            Question.objects.create(
+                author=request.user,
+                title=title,
+                content=content,
+                image=image
+            )
+            return redirect('questions_list')
+    return redirect('questions_list')
